5bafcb7d24d4dfff53fe6ddb1e9ab1cb1f190c39
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-17 23:52:46 -0300

    Create array-object.js

---
+++ array-object.js
@@ -0,0 +1,18 @@
+// Criando um array "users" de objetos e imprimindo atributos dos objetos.
+let users =[ 
+    {
+        name: "Calvin",
+        surname: "Hart",
+        age: 66,
+        email: "CalvinMHart@teleworm.us"
+    },
+    {
+        name: "Mateus",
+        surname: "Pinto",
+        age: 21,
+        email: "MateusPinto@dayrep.com"
+    }
+];
+
+console.log(users[0].name);
+console.log(users[1].age);
